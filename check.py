4c552344134c4f84e68fba07f2d27867e71457be
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-07-20 13:48:29 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,8 @@
 DECLARE
-  input_number NUMBER := 957562782.22; -- Replace this with your input number
+  input_number NUMBER := 78455.1155; -- Replace this with your input number
   output_number NUMBER;
 BEGIN
-  output_number := TO_NUMBER(SUBSTR(TO_CHAR(input_number), 1, CASE WHEN input_number >= 1000 THEN LENGTH(TO_CHAR(TRUNC(input_number))) - 3 ELSE LENGTH(TO_CHAR(TRUNC(input_number))) END));
+  output_number := TO_NUMBER(CASE WHEN input_number >= 1000 THEN SUBSTR(TO_CHAR(input_number), 1, LENGTH(TO_CHAR(TRUNC(input_number))) - 3) ELSE '0' END);
   DBMS_OUTPUT.PUT_LINE('Output: ' || output_number);
 END;
 /
