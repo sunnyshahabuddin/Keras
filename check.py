9147ab10376f55e22f60082d2c5e03dd61196cf3
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-07-25 05:10:08 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,58 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Chart, Point } from 'chart.js';
+import { Component, AfterViewInit } from '@angular/core';
+import { Chart, LineController, LineElement, PointElement, LinearScale, CategoryScale, Tooltip } from 'chart.js';
 
 @Component({
   selector: 'app-infocus',
   templateUrl: './infocus.component.html',
   styleUrls: ['./infocus.component.css']
 })
-export class InfocusComponent implements OnInit {
+export class InfocusComponent implements AfterViewInit {
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
     this.createLineChart();
   }
 
   createLineChart() {
-    const ctx = (document.getElementById('lineChart') as HTMLCanvasElement).getContext('2d');
-    const chart = new Chart(ctx, {
+    Chart.register(LineController, LineElement, PointElement, LinearScale, CategoryScale, Tooltip);
+
+    const ctx = document.getElementById('lineChart') as HTMLCanvasElement;
+    const lineChart = new Chart(ctx, {
       type: 'line',
       data: {
         labels: ['January 2023', 'February 2023'],
-        datasets: [{
-          label: 'Infocus Data',
-          data: [
-            { x: 'January 2023', y: 1.73 },
-            { x: 'February 2023', y: 1.68 }
-          ],
-          borderColor: 'red',
-          fill: false
-        }]
+        datasets: [
+          {
+            label: 'Infocus Line Chart',
+            data: [1.73, 1.68],
+            borderColor: 'red',
+            fill: false,
+          }
+        ]
       },
       options: {
         scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'month',
-              displayFormats: {
-                month: 'MMM YYYY'
-              }
-            },
-            ticks: {
-              source: 'labels'
-            }
-          },
           y: {
-            ticks: [1.67, 1.68, 1.69, 1.70, 1.71, 1.72, 1.73]
-          }
-        },
-        plugins: {
-          legend: {
-            display: true,
-            position: 'top'
+            beginAtZero: false,
+            ticks: {
+              stepSize: 0.01,
+              precision: 2,
+              min: 1.67,
+              max: 1.73
+            }
           }
         }
       }
     });
   }
+
 }
