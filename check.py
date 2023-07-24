4958156c143c891fadf7355a5cd697b942bab5f6
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-07-25 04:53:42 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,25 +1,58 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Chart, Point } from 'chart.js';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: 'app-infocus',
+  templateUrl: './infocus.component.html',
+  styleUrls: ['./infocus.component.css']
 })
-export class DashboardComponent {
-  data: any[] = [];
+export class InfocusComponent implements OnInit {
 
-  constructor(private http: HttpClient) {}
-
-  getData(): void {
-    this.getDataFromApi().subscribe((result: any) => {
-      this.data = result;
-    });
+  ngOnInit(): void {
+    this.createLineChart();
   }
 
-  getDataFromApi(): Observable<any> {
-    const apiUrl = 'http://localhost:3000/api/dashboard';
-    return this.http.get(apiUrl);
+  createLineChart() {
+    const ctx = (document.getElementById('lineChart') as HTMLCanvasElement).getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January 2023', 'February 2023'],
+        datasets: [{
+          label: 'Infocus Data',
+          data: [
+            { x: 'January 2023', y: 1.73 },
+            { x: 'February 2023', y: 1.68 }
+          ],
+          borderColor: 'red',
+          fill: false
+        }]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'month',
+              displayFormats: {
+                month: 'MMM YYYY'
+              }
+            },
+            ticks: {
+              source: 'labels'
+            }
+          },
+          y: {
+            ticks: [1.67, 1.68, 1.69, 1.70, 1.71, 1.72, 1.73]
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'top'
+          }
+        }
+      }
+    });
   }
 }
