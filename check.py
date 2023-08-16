027d9df098720719ce3e08fc3286e61918871725
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-08-16 15:07:08 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,12 +1,27 @@
-# Open the file in read mode
-with open('reg.env', 'r') as file:
-    # Read the lines from the file
-    lines = file.readlines()
+import pandas as pd
 
-# Iterate through the lines and find the one containing "logPath"
-for line in lines:
-    if 'export logPath' in line:
-        # Extract the value within single quotes
-        value = line.split("'")[1]
-        print(value)
-        break
+# Load the Excel file
+excel_file_path = 'path/to/your/excel/file.xlsx'
+df = pd.read_excel(excel_file_path)
+
+# Extract values from columns E and L
+column_e_values = df['E'].tolist()
+column_l_values = df['L'].tolist()
+
+# Find values in column E but not in column L
+e_not_in_l = [value for value in column_e_values if value not in column_l_values]
+
+# Find values in column L but not in column E
+l_not_in_e = [value for value in column_l_values if value not in column_e_values]
+
+# Create a DataFrame to store the results
+result_df = pd.DataFrame({
+    'E_not_in_L': e_not_in_l,
+    'L_not_in_E': l_not_in_e
+})
+
+# Save the DataFrame to a new Excel file
+output_excel_path = 'output.xlsx'
+result_df.to_excel(output_excel_path, index=False)
+
+print("Results saved to", output_excel_path)
