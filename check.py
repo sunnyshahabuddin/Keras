51212204e288c0837c8ad287f873ed1a1a988d1b
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-08-24 19:01:40 +0530

    Update check.py

--- check.py
+++ check.py
@@ -12,8 +12,21 @@ user_id = 'G025555'
 data['Reporting_Period'] = reporting_period
 data['User_id'] = user_id
 
+# Process columns with date values
+date_columns = [col for col in data.columns if 'date' in col.lower()]
+for col in date_columns:
+    data[col] = data[col].apply(lambda x: x.strftime('%d/%m/%Y') if pd.notnull(x) else '')
+
+# Format columns with leading zeros
+columns_with_zeros = ['Column1', 'Column2']  # Add column names here
+for col in columns_with_zeros:
+    data[col] = data[col].apply(lambda x: f'{x:0>7}' if pd.notnull(x) else '')
+
+# Replace NaN with blank
+data = data.where(pd.notna(data), '')
+
 # Create a pipe-separated .dat file
 dat_file_path = 'output.dat'
-data.to_csv(dat_file_path, sep='|', index=False)
+data.to_csv(dat_file_path, sep='|', index=False, na_rep='', date_format='%d/%m/%Y')
 
 print(f'{xlsx_file_path} has been converted to {dat_file_path}')
