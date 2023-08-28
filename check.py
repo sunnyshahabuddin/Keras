ab23877cfc031a93de95870d1053f981c380a7a5
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-08-29 01:03:26 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,19 +1,49 @@
-const request = require('request');
-const kerberos = require('kerberos');
+import mysql.connector
 
-const url = 'http://localhost:5000/api/v1.0/kerberos';
-const auth = new kerberos.HTTPKerberosAuth();
+# Assuming you have the 'data' variable containing the JSON data
+data = {
+    "data": {
+        "applicationProducViewData": [
+            {
+                "tc_id": "1",
+                "tc_name": "测试用例1",
+            },
+            {
+                "tc_id": "2",
+                "tc_name": "测试用例2",
+            },
+            {
+                "tc_id": "3",
+                "tc_name": "测试用例3",
+            }
+        ]
+    }
+}
 
-const options = {
-  url: url,
-  auth: auth,
-  verify: 'C:\Users\user\Documen\ca-7.pem'
-};
+# MySQL database configuration
+db_config = {
+    "host": "localhost",
+    "user": "your_username",
+    "password": "your_password",
+    "database": "test"
+}
 
-request.get(options, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(body);
-  }
-});
+# Establish a connection to the MySQL server
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
+
+# Extract the test cases from the JSON data and insert into the table
+test_cases = data["data"]["applicationProducViewData"]
+for test_case in test_cases:
+    tc_id = test_case["tc_id"]
+    tc_name = test_case["tc_name"]
+    
+    insert_query = "INSERT INTO test_data_table (id, name) VALUES (%s, %s)"
+    insert_data = (tc_id, tc_name)
+    
+    cursor.execute(insert_query, insert_data)
+    connection.commit()
+
+# Close the cursor and connection
+cursor.close()
+connection.close()
