a991a43bd54058649de7121580f78dd2eafeeb81
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-09-07 13:15:14 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,19 +1,32 @@
-# Convert the 'date' column to a datetime object (if not already done)
-data['date'] = pd.to_datetime(data['date'])
 
-# Set 'date' as the index
-data.set_index('date', inplace=True)
+Certainly, here's an explanation of the hyperparameters of a SARIMA (Seasonal Autoregressive Integrated Moving Average) model in simple terms:
 
-acf_values = sm.tsa.acf(data['close'], fft=False)
+p (Autoregressive Order - Non-seasonal):
 
-# Create a lag range for the x-axis
-lag_range = np.arange(0, len(acf_values))
+This parameter represents how much the current value of the time series depends on its past values.
+Higher values of p indicate that the current value is influenced by more past values.
+d (Differencing Order - Non-seasonal):
 
-# Create the ACF plot
-plt.figure(figsize=(10, 5))
-plt.bar(lag_range, acf_values)
-plt.xlabel('Lag')
-plt.ylabel('ACF Value')
-plt.title('Autocorrelation Function (ACF) Plot')
-plt.grid(True)
-plt.show()
+Differencing helps make the time series stationary (with a constant mean and variance).
+This parameter shows how many differences need to be taken to achieve stationarity.
+Higher values of d mean more differencing is required.
+q (Moving Average Order - Non-seasonal):
+
+This parameter represents how much the current value depends on past forecast errors.
+Higher values of q indicate that the current value is influenced by more past errors.
+P (Seasonal Autoregressive Order):
+
+Similar to p but for the seasonal part of the data.
+It represents the influence of past seasonal values on the current value.
+D (Seasonal Differencing Order):
+
+Similar to d but for the seasonal part of the data.
+It shows how many differences need to be taken on the seasonal component to achieve stationarity.
+Q (Seasonal Moving Average Order):
+
+Similar to q but for the seasonal part of the data.
+It represents the influence of past seasonal forecast errors on the current value.
+S (Seasonal Period):
+
+This parameter defines the number of time steps in a seasonal cycle.
+For example, if you have daily data with a weekly seasonality, S would be 7 because there are approximately 7 days in a week.
