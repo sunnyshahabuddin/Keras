3a000c07436e9361405ead30c3ae553119235439
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-09-07 05:30:21 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,54 +1,19 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from math import sqrt
-
-# Load the Apple share price dataset
-data = pd.read_csv('apple_share_prices.csv')
-
-# Convert the 'date' column to a datetime object
+# Convert the 'date' column to a datetime object (if not already done)
 data['date'] = pd.to_datetime(data['date'])
 
 # Set 'date' as the index
 data.set_index('date', inplace=True)
 
-# Group data by week and take the closing price as the target variable
-weekly_data = data['close'].resample('W').mean()
+acf_values = sm.tsa.acf(data['close'], fft=False)
 
-# Split data into train and test sets
-train_size = int(0.8 * len(weekly_data))
-train, test = weekly_data[:train_size], weekly_data[train_size:]
+# Create a lag range for the x-axis
+lag_range = np.arange(0, len(acf_values))
 
-# Define SARIMA hyperparameters (p, d, q, P, D, Q, S)
-p, d, q = 1, 1, 1  # Non-seasonal parameters
-P, D, Q, S = 1, 1, 1, 4  # Seasonal parameters (weekly data)
-
-# Fit the SARIMA model
-sarima_model = sm.tsa.SARIMA(train, order=(p, d, q), seasonal_order=(P, D, Q, S))
-sarima_result = sarima_model.fit()
-
-# Forecast using the trained model
-forecast = sarima_result.get_forecast(steps=len(test))
-
-# Calculate MAPE
-mape = np.mean(np.abs((test - forecast.predicted_mean) / test)) * 100
-
-# Calculate RMSE
-rmse = sqrt(mean_squared_error(test, forecast.predicted_mean))
-
-# Plot the actual vs. forecasted values
-plt.figure(figsize=(12, 6))
-plt.plot(train.index, train.values, label='Train Data')
-plt.plot(test.index, test.values, label='Test Data')
-plt.plot(forecast.predicted_mean.index, forecast.predicted_mean.values, label='Forecast')
-plt.legend()
-plt.title(f'SARIMA Forecast\nMAPE: {mape:.2f}% | RMSE: {rmse:.2f}')
-plt.xlabel('Date')
-plt.ylabel('Close Price')
+# Create the ACF plot
+plt.figure(figsize=(10, 5))
+plt.bar(lag_range, acf_values)
+plt.xlabel('Lag')
+plt.ylabel('ACF Value')
+plt.title('Autocorrelation Function (ACF) Plot')
+plt.grid(True)
 plt.show()
-
-# Print MAPE and RMSE
-print(f'MAPE: {mape:.2f}%')
-print(f'RMSE: {rmse:.2f}')
