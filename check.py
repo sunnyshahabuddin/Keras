eacf99c4a9e1da4b5e949561079b2831ff434a02
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-09-13 16:36:48 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,32 +1,38 @@
-
-Certainly, here's an explanation of the hyperparameters of a SARIMA (Seasonal Autoregressive Integrated Moving Average) model in simple terms:
-
-p (Autoregressive Order - Non-seasonal):
-
-This parameter represents how much the current value of the time series depends on its past values.
-Higher values of p indicate that the current value is influenced by more past values.
-d (Differencing Order - Non-seasonal):
-
-Differencing helps make the time series stationary (with a constant mean and variance).
-This parameter shows how many differences need to be taken to achieve stationarity.
-Higher values of d mean more differencing is required.
-q (Moving Average Order - Non-seasonal):
-
-This parameter represents how much the current value depends on past forecast errors.
-Higher values of q indicate that the current value is influenced by more past errors.
-P (Seasonal Autoregressive Order):
-
-Similar to p but for the seasonal part of the data.
-It represents the influence of past seasonal values on the current value.
-D (Seasonal Differencing Order):
-
-Similar to d but for the seasonal part of the data.
-It shows how many differences need to be taken on the seasonal component to achieve stationarity.
-Q (Seasonal Moving Average Order):
-
-Similar to q but for the seasonal part of the data.
-It represents the influence of past seasonal forecast errors on the current value.
-S (Seasonal Period):
-
-This parameter defines the number of time steps in a seasonal cycle.
-For example, if you have daily data with a weekly seasonality, S would be 7 because there are approximately 7 days in a week.
+{
+  "name": "usecase2",
+  "version": "0.1.0",
+  "private": true,
+  "dependencies": {
+    "@testing-library/jest-dom": "^5.17.0",
+    "@testing-library/react": "^13.4.0",
+    "@testing-library/user-event": "^13.5.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1",
+    "web-vitals": "^2.1.4"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "eslintConfig": {
+    "extends": [
+      "react-app",
+      "react-app/jest"
+    ]
+  },
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  }
+}
