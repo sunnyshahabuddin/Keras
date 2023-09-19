c70bfd071d7207956803b71486d4636b396b519c
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-09-20 05:02:30 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,34 @@
-const output = arr.reduce((uniqueItems, currentItem) => {
-  const name = currentItem.name;
-  if (!uniqueItems.some(item => item.name === name)) {
-    uniqueItems.push({ name, text: name });
-  }
-  return uniqueItems;
-}, []);
+import React from 'react';
+import XLSX from 'xlsx';
+
+const DownloadButton = ({ data, fileName }) => {
+  const downloadExcel = () => {
+    // Create a new workbook and add a worksheet
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(data);
+
+    // Add the worksheet to the workbook
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    // Create a Blob containing the workbook data
+    const blob = XLSX.write(workbook, { bookType: 'xlsx', type: 'blob' });
+
+    // Create a download link and trigger a click event to download the file
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName || 'data.xlsx';
+    a.click();
+
+    // Clean up
+    window.URL.revokeObjectURL(url);
+  };
+
+  return (
+    <button onClick={downloadExcel}>
+      Download Excel
+    </button>
+  );
+};
+
+export default DownloadButton;
