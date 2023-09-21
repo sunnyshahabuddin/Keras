3968db5800b7b10ef606f2b3f598b1d13c3c1491
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-09-21 14:46:18 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,37 +1,28 @@
-import pandas as pd
 import sqlite3
 
-# Define the Excel file and SQLite database file names
-excel_file = 'your_excel_file.xlsx'
-db_file = 'timeseries.db'
+# Replace 'your_database.db' with the actual path to your SQLite database file
+db_file = 'your_database.db'
 
-# Read the Excel data into a DataFrame
-df = pd.read_excel(excel_file, sheet_name='Sheet1')
-
-# Create a SQLite connection and cursor
+# Connect to the SQLite database
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-# Define the table name and create the table with auto-increment primary key
-table_name = 'timeseries_data'
-create_table_query = f'''
-CREATE TABLE IF NOT EXISTS {table_name} (
-    tab_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    {", ".join([f"{col_name} TEXT" for col_name in df.columns])}
-);
-'''
-cursor.execute(create_table_query)
+# Read SQL INSERT statements from the file
+with open('insert_statements.txt', 'r') as file:
+    insert_statements = file.read().split(';')
 
-# Insert data from the DataFrame into the SQLite table
-for index, row in df.iterrows():
-    insert_query = f'''
-    INSERT INTO {table_name} ({", ".join(df.columns)})
-    VALUES ({", ".join(["?" for _ in df.columns])});
-    '''
-    cursor.execute(insert_query, tuple(row))
+# Remove any empty statements
+insert_statements = [statement.strip() for statement in insert_statements if statement.strip()]
 
-# Commit the changes and close the database connection
-conn.commit()
+# Start a transaction
+conn.execute("BEGIN TRANSACTION;")
+
+# Execute the INSERT statements
+for statement in insert_statements:
+    cursor.execute(statement)
+
+# Commit the transaction to save changes to the database
+conn.execute("COMMIT;")
+
+# Close the database connection
 conn.close()
-
-print(f'Data from {excel_file} has been successfully imported into {db_file}.')
