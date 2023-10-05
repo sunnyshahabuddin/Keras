fe9c9aa7a41c13f36b85e7feeaba2bc97ffbf4d6
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-10-05 19:02:16 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,84 +1,47 @@
-Certainly, here are detailed points for each of the three pages in your EiE dashboard: Dashboard, Trend Report, and Ranking Page:
+Thank you for providing additional details about the Ranking page. Here's a more detailed breakdown of how the page works:
 
-*Dashboard Page:*
+*Ranking Page - Detailed Walkthrough:*
 
 1. *Introduction (1-2 minutes):*
-   - Welcome your audience to the Dashboard page.
-   - Mention that this page provides an overview of key metrics.
+   - Begin by introducing the Ranking page and its purpose.
+   - Explain that this page allows users to view rankings based on various criteria and themes.
 
-2. *Average Lead Time Tile (2-3 minutes):*
-   - Explain the first tile that displays the average lead time for each user story over the last 6 months.
-   - Discuss why this metric is important for evaluating project efficiency.
-   - Demonstrate how to interpret the data in this tile.
+2. *Date Selection (2-3 minutes):*
+   - Describe the "Select Date" dropdown.
+   - Explain that users can choose a date range, such as "Last Twelve Months," to view rankings for a specific period.
+   - Highlight the importance of selecting the right date range for accurate ranking analysis.
 
-3. *Granular Delivery Lead Names Tile (2-3 minutes):*
-   - Describe the second tile that shows granular delivery lead names and their teams' average lead times for the same period.
-   - Emphasize how this data provides insights into team performance.
-   - Walk through an example by clicking on a specific delivery lead's name to view more details.
+3. *Focus Selection (2-3 minutes):*
+   - Introduce the "Select Focus" dropdown.
+   - Explain that users can filter rankings based on whether an application is "In Focus," "Not In Focus," or view rankings for "All" applications.
+   - Discuss how this helps in evaluating specific subsets of applications.
 
-4. *Interactivity (2 minutes):*
-   - Highlight that the dashboard is interactive.
-   - Encourage your audience to click on elements for more information.
-   - Mention that they can customize the view to focus on specific data points.
+4. *Theme Selection (2-3 minutes):*
+   - Describe the "Select Theme" dropdown.
+   - Highlight that users can choose from five themes, such as "Version Control" and others.
+   - Mention that the metrics displayed will vary based on the selected theme.
 
-5. *Conclusion (1-2 minutes):*
-   - Summarize what was covered on the Dashboard page.
-   - Invite questions from the audience.
+5. *Metric Display (3-4 minutes):*
+   - Show the metrics configured for the selected theme. For example, in the case of "Version Control," there might be metrics like "Number of Commits," "Mapped Repos," "Number of Pull Requests," and "Percent Story Lineage."
+   - Explain the relevance of each metric and how they contribute to the theme's evaluation.
 
-*Trend Report Page:*
+6. *Ranking Display (4-5 minutes):*
+   - Explain that the page will generate two sets of divs: one for the top five applications and another for the bottom five applications based on the selected metrics and criteria.
+   - Demonstrate how the rankings are displayed, including the application name and its corresponding metric values.
 
-1. *Transition (1-2 minutes):*
-   - Transition from the Dashboard page to the Trend Report page.
-   - Explain that this page provides historical data trends.
+7. *User Interaction (2-3 minutes):*
+   - Encourage users to explore different date ranges, focus selections, and themes to view rankings for various scenarios.
+   - Mention that the rankings dynamically update based on user selections.
 
-2. *Select Date Range (2-3 minutes):*
-   - Show the Date Range dropdown and its options (e.g., 2 months, 4 months, 6 months, 12 months).
-   - Explain the importance of choosing the right date range for trend analysis.
+8. *Conclusion (1-2 minutes):*
+   - Summarize the key features and capabilities of the Ranking page.
+   - Remind users that this tool provides valuable insights for assessing application performance.
+  
+9. *Q&A (5-10 minutes):*
+   - Open the floor for questions and address any queries or feedback from your audience.
+  
+10. *Closing Remarks (1-2 minutes):*
+   - Thank your audience for their participation and attention.
+   - Provide contact information for further inquiries or support.
 
-3. *Display Trends (3-4 minutes):*
-   - Demonstrate how to view trends for specific metrics.
-   - Interact with the chart or graph to illustrate changes over time.
-   - Discuss how trend analysis can help in making informed decisions.
-
-4. *Filter by Theme (2-3 minutes):*
-   - Introduce the Theme filter and its purpose.
-   - Show how to filter trends based on a selected theme (e.g., Version Control or Outcome Metrics).
-   - Explain how this can provide thematic insights.
-
-5. *User Interaction (2 minutes):*
-   - Encourage the audience to explore different date ranges and themes.
-   - Mention that they can export trend data for further analysis.
-
-6. *Conclusion (1-2 minutes):*
-   - Summarize what was covered on the Trend Report page.
-   - Invite questions from the audience.
-
-*Ranking Page:*
-
-1. *Transition (1-2 minutes):*
-   - Transition from the Trend Report page to the Ranking page.
-   - Explain that this page provides rankings based on specific criteria.
-
-2. *Select Ranking Criteria (2-3 minutes):*
-   - Show the dropdown for selecting ranking criteria (e.g., Performance, Efficiency).
-   - Discuss why these criteria are important for assessing project performance.
-
-3. *View Rankings (3-4 minutes):*
-   - Demonstrate how to view rankings for specific components or teams.
-   - Interact with the ranking table to showcase top performers.
-   - Explain how rankings can help in identifying strengths and areas for improvement.
-
-4. *Filter by Area (2-3 minutes):*
-   - Introduce the Area filter and its purpose.
-   - Show how to filter rankings based on a selected area (e.g., 'Jacquie' area).
-   - Discuss how this can provide insights by region or department.
-
-5. *User Interaction (2 minutes):*
-   - Encourage the audience to explore different ranking criteria and areas.
-   - Mention that they can delve deeper into specific rankings.
-
-6. *Conclusion (1-2 minutes):*
-   - Summarize what was covered on the Ranking page.
-   - Invite questions from the audience.
-
-Remember to adapt the timing and level of detail to suit your audience's familiarity with the dashboard and their specific interests or questions.
+Ensure that your demonstration is clear and interactive, allowing users to see how rankings change based on different selections. Adapt the timing as needed to accommodate questions and discussions during the demo.
