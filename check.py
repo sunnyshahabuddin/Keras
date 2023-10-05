5dd115126fa53791f92d91e9906c9e89dc707c2f
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-10-05 18:51:27 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,11 +1,84 @@
-import json
+Certainly, here are detailed points for each of the three pages in your EiE dashboard: Dashboard, Trend Report, and Ranking Page:
 
-# Read the JSON data from the file
-with open('data.json', 'r') as json_file:
-    data = json.load(json_file)
+*Dashboard Page:*
 
-# Extract the "sm_Business_Product_Name" values for objects with "sm_Buss_CDR" as "Jack Jones"
-jack_jones_products = [item['sm_Business_Product_Name'] for item in data['applicationProductViewData'] if item['sm_Buss_CDR'] == 'Jack Jones']
+1. *Introduction (1-2 minutes):*
+   - Welcome your audience to the Dashboard page.
+   - Mention that this page provides an overview of key metrics.
 
-# Print the list of products for Jack Jones
-print(jack_jones_products)
+2. *Average Lead Time Tile (2-3 minutes):*
+   - Explain the first tile that displays the average lead time for each user story over the last 6 months.
+   - Discuss why this metric is important for evaluating project efficiency.
+   - Demonstrate how to interpret the data in this tile.
+
+3. *Granular Delivery Lead Names Tile (2-3 minutes):*
+   - Describe the second tile that shows granular delivery lead names and their teams' average lead times for the same period.
+   - Emphasize how this data provides insights into team performance.
+   - Walk through an example by clicking on a specific delivery lead's name to view more details.
+
+4. *Interactivity (2 minutes):*
+   - Highlight that the dashboard is interactive.
+   - Encourage your audience to click on elements for more information.
+   - Mention that they can customize the view to focus on specific data points.
+
+5. *Conclusion (1-2 minutes):*
+   - Summarize what was covered on the Dashboard page.
+   - Invite questions from the audience.
+
+*Trend Report Page:*
+
+1. *Transition (1-2 minutes):*
+   - Transition from the Dashboard page to the Trend Report page.
+   - Explain that this page provides historical data trends.
+
+2. *Select Date Range (2-3 minutes):*
+   - Show the Date Range dropdown and its options (e.g., 2 months, 4 months, 6 months, 12 months).
+   - Explain the importance of choosing the right date range for trend analysis.
+
+3. *Display Trends (3-4 minutes):*
+   - Demonstrate how to view trends for specific metrics.
+   - Interact with the chart or graph to illustrate changes over time.
+   - Discuss how trend analysis can help in making informed decisions.
+
+4. *Filter by Theme (2-3 minutes):*
+   - Introduce the Theme filter and its purpose.
+   - Show how to filter trends based on a selected theme (e.g., Version Control or Outcome Metrics).
+   - Explain how this can provide thematic insights.
+
+5. *User Interaction (2 minutes):*
+   - Encourage the audience to explore different date ranges and themes.
+   - Mention that they can export trend data for further analysis.
+
+6. *Conclusion (1-2 minutes):*
+   - Summarize what was covered on the Trend Report page.
+   - Invite questions from the audience.
+
+*Ranking Page:*
+
+1. *Transition (1-2 minutes):*
+   - Transition from the Trend Report page to the Ranking page.
+   - Explain that this page provides rankings based on specific criteria.
+
+2. *Select Ranking Criteria (2-3 minutes):*
+   - Show the dropdown for selecting ranking criteria (e.g., Performance, Efficiency).
+   - Discuss why these criteria are important for assessing project performance.
+
+3. *View Rankings (3-4 minutes):*
+   - Demonstrate how to view rankings for specific components or teams.
+   - Interact with the ranking table to showcase top performers.
+   - Explain how rankings can help in identifying strengths and areas for improvement.
+
+4. *Filter by Area (2-3 minutes):*
+   - Introduce the Area filter and its purpose.
+   - Show how to filter rankings based on a selected area (e.g., 'Jacquie' area).
+   - Discuss how this can provide insights by region or department.
+
+5. *User Interaction (2 minutes):*
+   - Encourage the audience to explore different ranking criteria and areas.
+   - Mention that they can delve deeper into specific rankings.
+
+6. *Conclusion (1-2 minutes):*
+   - Summarize what was covered on the Ranking page.
+   - Invite questions from the audience.
+
+Remember to adapt the timing and level of detail to suit your audience's familiarity with the dashboard and their specific interests or questions.
