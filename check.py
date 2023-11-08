4afca291fb7b9b8ce98ed1d98ef23963e5bee26d
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-11-08 11:49:09 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,11 @@
-[System.Environment]::SetEnvironmentVariable("npm_config_userconfig", "<path_to_your_custom_npmrc_directory>\npmrc", [System.EnvironmentVariableTarget]::User)
+Hyperparameter tuning, also known as hyperparameter optimization or hyperparameter search, is the process of finding the best set of hyperparameters for a machine learning model. Hyperparameters are configurations and settings that are not learned from the training data but are set prior to training the model. These hyperparameters significantly influence the model's performance and its ability to generalize to new, unseen data.
+
+Examples of hyperparameters in machine learning models include:
+
+Learning rate in gradient descent algorithms.
+Number of hidden layers and units in a neural network.
+Number of trees in a random forest.
+Depth of a decision tree.
+Regularization strength in linear models.
+Batch size during training.
+Kernel type and hyperparameters in support vector machines.
