d69208b717a10697e4f1b33f604660df746d26fc
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-11-13 10:45:05 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,4 @@
-
-Sure, let's break down hyperparameter tuning in simpler terms:
-
-Imagine you're baking cookies, and you have a recipe with a few variables you can change, like the amount of sugar and baking time. These variables are like "hyperparameters" in a recipe. The right combination of these variables can make your cookies taste the best.
-
-Hyperparameter tuning is like experimenting with different amounts of sugar and baking times to find the perfect combination that makes the most delicious cookies. You might try a little more sugar or bake them a bit longer each time and taste the results. The goal is to figure out the best sugar amount and baking time to make your cookies taste just right.
-
-In machine learning, models have their own "recipe" with settings that need to be adjusted, such as learning rate or the number of layers in a neural network. Hyperparameter tuning is the process of trying out different settings for these model ingredients to make the model perform as well as possible on its task, just like finding the perfect cookie recipe.
+SELECT C.COLUMN_NAME, P.PARTITION_NAME
+FROM USER_TAB_COLUMNS C
+JOIN USER_TAB_PARTITIONS P ON C.TABLE_NAME = P.TABLE_NAME
+WHERE C.TABLE_NAME = 'your_table_name';
