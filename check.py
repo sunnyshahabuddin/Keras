1bf8b94982b5a71ce3689e904c3ed7f748f1036d
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-11-15 15:30:50 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,4 +1,12 @@
-SELECT C.COLUMN_NAME, P.PARTITION_NAME
-FROM USER_TAB_COLUMNS C
-JOIN USER_TAB_PARTITIONS P ON C.TABLE_NAME = P.TABLE_NAME
-WHERE C.TABLE_NAME = 'your_table_name';
+from datetime import datetime, timedelta
+
+current_date = datetime.now()
+start_of_month = current_date.replace(day=1)
+next_month = start_of_month.replace(month=start_of_month.month + 1, day=1)
+end_of_month = next_month - timedelta(days=1)
+
+start_of_month_str = start_of_month.strftime('%Y-%m-%d')
+end_of_month_str = end_of_month.strftime('%Y-%m-%d')
+
+print(f"Start date of the current month: {start_of_month_str}")
+print(f"End date of the current month: {end_of_month_str}")
