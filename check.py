1844949f13e54c8fcbc260f6c2c2abbd7a4db2a1
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-11-25 02:00:32 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,6 +1,20 @@
--- Check if the adjustment table exists for the specified data source
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = {{sourceAdjustmentTable('ds_enrich_missing', @EXEC_DATE)}})
+DECLARE
+   table_name VARCHAR2(30) := 'YOUR_TABLE_NAME'; -- Replace with the actual table name
+
+   table_count NUMBER;
 BEGIN
-    -- If the adjustment table exists, then execute the DELETE statement
-    DELETE FROM {{sourceAdjustmentTable('ds_enrich_missing', @EXEC_DATE)}};
-END
+   -- Check if the table exists
+   SELECT COUNT(*)
+   INTO table_count
+   FROM USER_TABLES
+   WHERE TABLE_NAME = table_name;
+
+   -- If the table exists, delete it
+   IF table_count > 0 THEN
+      EXECUTE IMMEDIATE 'DROP TABLE ' || table_name;
+      DBMS_OUTPUT.PUT_LINE('Table ' || table_name || ' has been deleted.');
+   ELSE
+      DBMS_OUTPUT.PUT_LINE('Table ' || table_name || ' does not exist.');
+   END IF;
+END;
+/
