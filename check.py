eab4891e0a5b6ce28aa7451395a22bcf04eed65f
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2023-12-06 00:06:12 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,20 +1,20 @@
-DECLARE
-   table_name VARCHAR2(30) := 'YOUR_TABLE_NAME'; -- Replace with the actual table name
-
-   table_count NUMBER;
-BEGIN
-   -- Check if the table exists
-   SELECT COUNT(*)
-   INTO table_count
-   FROM USER_TABLES
-   WHERE TABLE_NAME = table_name;
-
-   -- If the table exists, delete it
-   IF table_count > 0 THEN
-      EXECUTE IMMEDIATE 'DROP TABLE ' || table_name;
-      DBMS_OUTPUT.PUT_LINE('Table ' || table_name || ' has been deleted.');
-   ELSE
-      DBMS_OUTPUT.PUT_LINE('Table ' || table_name || ' does not exist.');
-   END IF;
-END;
-/
+def add_today_values(predictions, today_values):
+    # loop through the predictions
+    for prediction in predictions:
+        # get the metric name from the prediction
+        metric_name = prediction['metric_name']
+        # get the today value from the today_values dictionary
+        today_value = today_values[metric_name]
+        # add the today value to the prediction
+        prediction['today_value'] = today_value
+    
+    for prediction in predictions:
+        # add a new key threshold to the prediction which is plus minus 5% of the today value in the format [0.95, 1.05]]
+        prediction['threshold'] = [prediction['today_value'] * 0.95, prediction['today_value'] * 1.05]
+        # now add a new key called exception which will have value 'Yes' if the prediction value is outside the threshold and 'No' if it is within the threshold
+        if prediction['value'] < prediction['threshold'][0] or prediction['value'] > prediction['threshold'][1]:
+            prediction['exception'] = 'Yes'
+        else:
+            prediction['exception'] = 'No'
+    # return the predictions
+    return predictions
