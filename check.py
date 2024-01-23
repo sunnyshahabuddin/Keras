c9fd85ce6968bc452e0cacef618b2ac0231d3e7b
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2024-01-24 02:41:46 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,29 +1,130 @@
-import re
+<p>Sort Table Rows by Clicking on the Table Headers - Ascending and Descending (jQuery)</p>
+<div class="container">
+	
+	<div class="table">
+		<div class="table-header">
+			<div class="header__item"><a id="name" class="filter__link" href="#">Name</a></div>
+			<div class="header__item"><a id="wins" class="filter__link filter__link--number" href="#">Wins</a></div>
+			<div class="header__item"><a id="draws" class="filter__link filter__link--number" href="#">Draws</a></div>
+			<div class="header__item"><a id="losses" class="filter__link filter__link--number" href="#">Losses</a></div>
+			<div class="header__item"><a id="total" class="filter__link filter__link--number" href="#">Total</a></div>
+		</div>
+		<div class="table-content">	
+			<div class="table-row">		
+				<div class="table-data">Tom</div>
+				<div class="table-data">2</div>
+				<div class="table-data">0</div>
+				<div class="table-data">1</div>
+				<div class="table-data">5</div>
+			</div>
+			<div class="table-row">
+				<div class="table-data">Dick</div>
+				<div class="table-data">1</div>
+				<div class="table-data">1</div>
+				<div class="table-data">2</div>
+				<div class="table-data">3</div>
+			</div>
+			<div class="table-row">
+				<div class="table-data">Harry</div>
+				<div class="table-data">0</div>
+				<div class="table-data">2</div>
+				<div class="table-data">2</div>
+				<div class="table-data">2</div>
+			</div>
+		</div>	
+	</div>
+</div>
 
-def convert_shell_to_python(shell_script):
-    # Define conversion rules using regular expressions
-    conversion_rules = [
-        (r'\$(\w+)', r'os.environ.get("\1")'),  # Replace environment variables
-        (r'\becho\b', 'print'),                  # Replace echo with print
-        # Add more rules as needed
-    ]
 
-    # Apply conversion rules to the shell script
-    python_script = shell_script
-    for pattern, replacement in conversion_rules:
-        python_script = re.sub(pattern, replacement, python_script)
 
-    return python_script
 
-# Example shell script
-shell_script = """
-#!/bin/bash
-echo "Hello, world!"
-echo "My environment variable is: $MY_VAR"
-"""
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
 
-# Convert the shell script to Python
-python_script = convert_shell_to_python(shell_script)
+$base-spacing-unit: 24px;
+$half-spacing-unit: $base-spacing-unit / 2;
 
-# Print the resulting Python script
-print(python_script)
+$color-alpha: #1772FF;
+$color-form-highlight: #EEEEEE;
+
+*, *:before, *:after {
+	box-sizing:border-box;
+}
+
+body {
+	padding:$base-spacing-unit;
+	font-family:'Source Sans Pro', sans-serif;
+	margin:0;
+}
+
+h1,h2,h3,h4,h5,h6 {
+	margin:0;
+}
+
+.container {
+	max-width: 1000px;
+	margin-right:auto;
+	margin-left:auto;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	min-height:100vh;
+}
+
+.table {
+	width:100%;
+	border:1px solid $color-form-highlight;
+}
+
+.table-header {
+	display:flex;
+	width:100%;
+	background:#000;
+	padding:($half-spacing-unit * 1.5) 0;
+}
+
+.table-row {
+	display:flex;
+	width:100%;
+	padding:($half-spacing-unit * 1.5) 0;
+	
+	&:nth-of-type(odd) {
+		background:$color-form-highlight;
+	}
+}
+
+.table-data, .header__item {
+	flex: 1 1 20%;
+	text-align:center;
+}
+
+.header__item {
+	text-transform:uppercase;
+}
+
+.filter__link {
+	color:white;
+	text-decoration: none;
+	position:relative;
+	display:inline-block;
+	padding-left:$base-spacing-unit;
+	padding-right:$base-spacing-unit;
+	
+	&::after {
+		content:'';
+		position:absolute;
+		right:-($half-spacing-unit * 1.5);
+		color:white;
+		font-size:$half-spacing-unit;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+	
+	&.desc::after {
+		content: '(desc)';
+	}
+
+	&.asc::after {
+		content: '(asc)';
+	}
+	
+}
