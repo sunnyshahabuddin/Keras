9c855ab06f0294b1044b83c853f8bbfa0bcdeb49
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2024-08-27 00:45:06 +0530

    Update check.py

--- check.py
+++ check.py
@@ -12,7 +12,7 @@ for index, row in df.iterrows():
     if isinstance(row[0], pd.Timestamp):
         date_value = row[0].strftime('%d/%m/%Y')
     else:
-        date_value = str(row[0]).replace("'", "''")
+        date_value = row[0].replace("'", "''")
     
     other_values = []
     for i, value in enumerate(row[1:]):
@@ -20,12 +20,11 @@ for index, row in df.iterrows():
             if value.isnumeric():
                 other_values.append(str(int(value)))
             else:
-                other_values.append(f"'{str(value).replace("'", "''")}'")
+                other_values.append(f"{value.replace("'", "''")}")
         else:
-            other_values.append(f"'{str(value).replace("'", "''")}'")
+            other_values.append(f"'{value.replace("'", "''")}'")
     
     other_values_str = ', '.join(other_values)
-    
     insert_statement = f"INSERT INTO {table_name} VALUES ('{date_value}', {other_values_str});"
     insert_statements.append(insert_statement)
 
