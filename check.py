6d5c14c90315a6ed6fe748b558943e75256564e7
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2024-08-27 00:38:37 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-file_path = 'test.xlsx'
-output_file_path = 'insert_statement.sql'
+file_path = r'C:\user\initial_data.xlsx'
 
 df = pd.read_excel(file_path, dtype=str, parse_dates=[0])
 
@@ -15,11 +14,22 @@ for index, row in df.iterrows():
     else:
         date_value = str(row[0]).replace("'", "''")
     
-    other_values = "', '".join(str(value).replace("'", "''") for value in row[1:])
+    other_values = []
+    for i, value in enumerate(row[1:]):
+        if i == len(row[1:]) - 1:
+            if value.isnumeric():
+                other_values.append(str(int(value)))
+            else:
+                other_values.append(f"'{str(value).replace("'", "''")}'")
+        else:
+            other_values.append(f"'{str(value).replace("'", "''")}'")
     
-    insert_statement = f"INSERT INTO {table_name} VALUES ('{date_value}', '{other_values}');"
+    other_values_str = ', '.join(other_values)
+    
+    insert_statement = f"INSERT INTO {table_name} VALUES ('{date_value}', {other_values_str});"
     insert_statements.append(insert_statement)
 
+output_file_path = r'C:\user\insert_statements.sql'
 with open(output_file_path, 'w') as file:
     file.write('\n'.join(insert_statements))
 
